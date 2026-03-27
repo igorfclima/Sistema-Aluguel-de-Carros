56f4d35c37bc99f2d4ae7d721e31cd1eeaeefd8f
Igor <igrfclima@gmail.com>
2026-03-27 14:24:16 -0300

    fix: add agente and banco dto

--- backend/internal/dto/usuario_dto.go
+++ backend/internal/dto/usuario_dto.go
@@ -1,13 +1,21 @@
 package dto
 
 type CreateUsuarioRequest struct {
+	//user
 	Nome  string `json:"nome" binding:"required"`
 	Email string `json:"email" binding:"required,email"`
 	Senha string `json:"senha" binding:"required,min=6"`
 	Tipo  string `json:"tipo" binding:"required"`
 
+	//cliebte
 	CPF       string `json:"cpf"`
 	RG        string `json:"rg"`
 	Endereco  string `json:"endereco"`
 	Profissao string `json:"profissao"`
+
+	//agente
+	TipoAgente      string `json:"tipo_agente"`
+	NomeInstituicao string `json:"nome_instituicao"`
+	//banco
+	CodigoBancario string `json:"codigo_bancario"`
 }

59a5d9415333e2f72f9189c627f859ff29fa6ae1
Igor <igrfclima@gmail.com>
2026-03-27 14:02:30 -0300

    feat: expand request of the dto to accept client data

--- backend/internal/dto/usuario_dto.go
+++ backend/internal/dto/usuario_dto.go
@@ -5,4 +5,9 @@ type CreateUsuarioRequest struct {
 	Email string `json:"email" binding:"required,email"`
 	Senha string `json:"senha" binding:"required,min=6"`
 	Tipo  string `json:"tipo" binding:"required"`
+
+	CPF       string `json:"cpf"`
+	RG        string `json:"rg"`
+	Endereco  string `json:"endereco"`
+	Profissao string `json:"profissao"`
 }

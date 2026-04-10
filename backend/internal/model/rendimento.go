438828fc2f659dd77011555fe9bb4b83a858d9ad
Igor <igrfclima@gmail.com>
2026-04-10 20:07:41 -0300

    roda

--- backend/internal/model/rendimento.go
+++ backend/internal/model/rendimento.go
@@ -2,7 +2,7 @@ package model
 
 type Rendimento struct {
 	ID        uint    `gorm:"primaryKey"`
-	ClienteID uint    `gorm:"not null"`
+	ClienteID uint    `gorm:"not null;index"`
 	Valor     float64 `gorm:"not null"`
 	Periodo   string  `gorm:"size:50;not null"`
 }
